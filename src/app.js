39a895fac05f6246befd657739d1d5c2c078ac64
CamilaGonzalezBrito <115099889+CamilaGonzalezBrito@users.noreply.github.com>
2026-02-13 23:23:16 +0000

    Random Card

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateCard(){
+  let cardValue = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let suit = ["Diamonds", "Spades", "Hearts", "Clubs"];
+  let randomCardNumber = Math.floor(Math.random() * cardValue.length);
+  let randomSuitNumber = Math.floor(Math.random() * suit.length);
+  let result = suit[randomSuitNumber];
+
+  const cardContentEl = document.getElementById('cardCont');
+  const cardEl = document.getElementById('theCard');
+
+  cardContentEl.textContent = cardValue[randomCardNumber];
+  cardEl.className = `card ${getSuiteClass(result)}`;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateCard()
+  
+  document.getElementById("newCardBtn")
+  document.addEventListener("click", generateCard);
 };
+
+function getSuiteClass(suit) {
+  switch (suit) {
+    case "Diamonds": return "suit-diamonds";
+    case "Spades": return "suit-spades";
+    case "Hearts": return "suit-hearts";
+    case "Clubs": return "suit-clubs";
+  }
+}
